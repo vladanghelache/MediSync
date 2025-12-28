8f56cc1b284487ec96c632ce04acd0cbf92935b5
Vlad Anghelache <anghelache.vlad.1@gmail.com>
2025-12-28 19:27:48 +0200

    Add swagger

---
+++ src/main/java/com/medisync/MediSync/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.medisync.MediSync.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI medisyncOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("MediSync API")
+                        .description("Hospital Management System API for managing doctors, patients, and appointments.")
+                        .version("1.0"));
+
+    }
+}
