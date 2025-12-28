000569ec2298198d356f17fa43b750dede69b52e
Vlad Anghelache <anghelache.vlad.1@gmail.com>
2025-12-28 17:53:40 +0200

    First commit

---
+++ src/main/java/com/medisync/MediSync/MediSyncApplication.java
@@ -0,0 +1,13 @@
+package com.medisync.MediSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediSyncApplication.class, args);
+	}
+
+}
