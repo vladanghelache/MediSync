63431215de0cf537f1c720bd9c537905bf37da67
Vlad Anghelache <anghelache.vlad.1@gmail.com>
2025-12-30 03:03:22 +0200

    Remove unused UserRepository from AuthService

--- src/main/java/com/medisync/MediSync/service/AuthService.java
+++ src/main/java/com/medisync/MediSync/service/AuthService.java
@@ -2,20 +2,16 @@ package com.medisync.MediSync.service;
 
 import com.medisync.MediSync.dto.AuthTokenDto;
 import com.medisync.MediSync.dto.CredentialsDto;
-import com.medisync.MediSync.entity.User;
-import com.medisync.MediSync.repository.UserRepository;
 import com.medisync.MediSync.security.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
-    private final UserRepository userRepository;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
 
